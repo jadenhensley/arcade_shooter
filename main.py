92fdf964255058ecf78c27352b928f01c8a26066
jadenhensley <jadenhensley@github.com>
2022-01-21 20:43:50 -0500

    finished game

--- main.py
+++ main.py
@@ -20,6 +20,16 @@ ENEMY_IMAGES = [
     pygame.image.load(f"{PROJECT_PATH}/sprites/enemyGreen1.png"),
     pygame.image.load(f"{PROJECT_PATH}/sprites/enemyRed1.png"),
 ]
+METEOR_IMAGES = [
+    pygame.image.load(f"{PROJECT_PATH}/sprites/meteorBrown_big1.png"),
+    pygame.image.load(f"{PROJECT_PATH}/sprites/meteorBrown_big2.png"),
+    pygame.image.load(f"{PROJECT_PATH}/sprites/meteorBrown_big3.png"),
+    pygame.image.load(f"{PROJECT_PATH}/sprites/meteorBrown_big4.png"),
+    pygame.image.load(f"{PROJECT_PATH}/sprites/meteorGrey_med1.png"),
+    pygame.image.load(f"{PROJECT_PATH}/sprites/meteorGrey_med2.png"),
+    pygame.image.load(f"{PROJECT_PATH}/sprites/meteorGrey_small1.png"),
+    pygame.image.load(f"{PROJECT_PATH}/sprites/meteorGrey_small2.png")
+]
 
 PLAYER_IMG = pygame.image.load(f"{PROJECT_PATH}/sprites/playerShip3_red.png")
 P_LASER_IMG = pygame.image.load(f"{PROJECT_PATH}/sprites/laserBlue02.png")
@@ -38,16 +48,51 @@ def printgLarge(text, x, y, color=(255,255,255)):
     text = monogramXL.render(text, True, color)
     screen.blit(text, (x, y))
 
+laser_group = []
+
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = random.choice(ENEMY_IMAGES)
+        self.image = pygame.transform.scale(self.image, (self.image.get_width()//2, self.image.get_height()//2))
         self.rect = self.image.get_rect()
         self.rect.top = 100
         self.rect.left = random.randint(100, sWidth - 100)
+        self.direction = "right"
         
     def update(self):
         screen.blit(self.image, self.rect.topleft)
+        if self.direction == "right":
+            self.rect.right += random.randint(10,20)
+        if self.direction == "left":
+            self.rect.left -= random.randint(10,10)
+        if self.rect.left < 0:
+            self.direction = "right"
+            self.rect.y += 50
+        if self.rect.right > sWidth:
+            self.direction = "left"
+            self.rect.y += 50
+        # if self.rect.colliderect(PlayerLaser()):
+        for laser in laser_group:
+            if self.rect.colliderect(laser.rect):
+                p.score += 1
+                self.kill()
+        if self.rect.colliderect(p.rect):
+            p.kill()
+            g.current_scene = "game_over"
+
+class Meteor(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = random.choice(METEOR_IMAGES)
+        self.rect = self.image.get_rect()
+        self.rect.centery = random.randint(0, sHeight // 2)
+        self.rect.centerx = random.randint(0, sWidth // 2)
+    
+    def update(self):
+        screen.blit(self.image, self.rect.topleft)
+        self.rect.bottom += 10
+        self.rect.left += 10
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -55,10 +100,12 @@ class Player(pygame.sprite.Sprite):
         self.image = PLAYER_IMG
         self.rect = self.image.get_rect()
         self.rect.bottom = sHeight - 100
+        self.rect.centerx = sWidth // 2 - 30
         self.RIGHT, self.LEFT, self.SPACE = False, False, False
         self.lasercount = 0
-        self.max_lasers = 3
+        self.max_lasers = 8
         self.lastTick = 0
+        self.score = 0
 
     def update(self):
         screen.blit(self.image, self.rect.topleft)
@@ -68,17 +115,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.left -= 10
         if self.SPACE:
             if self.lasercount < self.max_lasers:
-                e.add(PlayerLaser())
+                temp = PlayerLaser()
+                e.add(temp)
+                laser_group.append(temp)
                 self.lasercount += 1
-                print(self.lasercount)
-            elif self.lasercount == self.max_lasers:
-                self.lastTick = pygame.time.get_ticks()
-                self.lasercount += 1
-                print(self.lasercount)
-        
-        if pygame.time.get_ticks() > self.lastTick + 1000 and self.SPACE == True:
-            self.lasercount = 0
-            # cannot spam laser
 
 class PlayerLaser(pygame.sprite.Sprite):
     def __init__(self):
@@ -91,8 +131,7 @@ class PlayerLaser(pygame.sprite.Sprite):
     def update(self):
         screen.blit(self.image, self.rect.topleft)
         self.rect.bottom -= 10
-        if self.rect.colliderect(Enemy().rect):
-            pass
+        
 
 class EnemyLaser(pygame.sprite.Sprite):
     def __init__(self):
@@ -131,6 +170,7 @@ class Game:
 
         screen.fill((40,40,40))
         printgLarge("Arcade Shooter", sWidth // 4, sHeight // 2, (0,255,0))
+        printgLarge("Press Enter/Return key to start.", sWidth // 8, sHeight // 2 + 50, (255,255,255))
         clock.tick(60)
         pygame.display.update()
         pygame.display.flip() # forgot
@@ -149,8 +189,6 @@ class Game:
                 if event.key == pygame.K_SPACE:
                     p.SPACE = True
             if event.type == pygame.KEYUP: # KEYUP / KEYDOWN, not pygame.key.get_pressed()
-                if event.key == pygame.K_RETURN:
-                    self.current_scene = "game_over"
                 if event.key == pygame.K_LEFT:
                     p.LEFT = False
                 if event.key == pygame.K_RIGHT:
@@ -159,10 +197,23 @@ class Game:
                     p.SPACE = False
 
         screen.fill((40,40,40))
-        printgLarge("In Game", sWidth // 4, sHeight // 2, (0,255,0))
+        clock.tick(60)
+
+        ticks = pygame.time.get_ticks()
+        if ticks % 50 == 0:
+            e.add(Enemy())
+            p.lasercount = 0
+        if ticks % 100 == 0:
+            e.add(Enemy())
+            p.lasercount = 0
+        if ticks % 5 == 0:
+            e.add(Meteor())
+
+
         p.update()
         e.update()
-        clock.tick(60)
+
+        printgLarge(f"score: {p.score}", sWidth // 12, 10, (255,215,40))
         pygame.display.update()
         pygame.display.flip() # forgot
 
@@ -178,6 +229,7 @@ class Game:
 
         screen.fill((40,40,40))
         printgLarge("Game Over", sWidth // 4, sHeight // 2, (0,255,0))
+        printgLarge(f"Your Score: {p.score}", sWidth // 4 - 20, sHeight // 2 + 100, (255,255,255))
         clock.tick(60)
         pygame.display.update()
         pygame.display.flip() # forgot
